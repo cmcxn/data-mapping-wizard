5be99d214e074a42242bc3afa70dba029afc5455
chenmins <chenmins@88.com>
2025-03-11 11:32:10 +0800

    最大化和 dpi 问题

--- src/main/java/com/datamap/Main.java
+++ src/main/java/com/datamap/Main.java
@@ -2,6 +2,7 @@ package com.datamap;
 
 import com.datamap.ui.DataMapWizard;
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,8 +15,27 @@ public class Main {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                
+
+                // Get screen DPI
+                GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+                DisplayMode dm = gd.getDisplayMode();
+                int screenDPI = Toolkit.getDefaultToolkit().getScreenResolution();
+                double scale = screenDPI / 96.0; // Assuming 96 DPI as standard
+
+                // Create and set up the wizard frame
                 DataMapWizard wizard = new DataMapWizard();
+                wizard.setPreferredSize(new Dimension((int) (800 * scale), (int) (600 * scale)));
+                wizard.pack();
+
+                // Center the frame on screen
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                int x = (screenSize.width - wizard.getWidth()) / 2;
+                int y = (screenSize.height - wizard.getHeight()) / 2;
+                wizard.setLocation(x, y);
+
+                // Maximize the window
+                wizard.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
                 wizard.setVisible(true);
             }
         });
